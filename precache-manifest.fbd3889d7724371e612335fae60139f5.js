78b08e936f054790b10946f1cfcaac7502587c03
Zzoda Turtle <63571593+Jiyeong-Oh@users.noreply.github.com>
2022-12-16 21:54:54 +0900

    Add files via upload

---
+++ precache-manifest.fbd3889d7724371e612335fae60139f5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3884863fb0fb7ec9807",
+    "url": "/moolalahdigitalportfolio/static/css/main.6b68cc24.chunk.css"
+  },
+  {
+    "revision": "f3884863fb0fb7ec9807",
+    "url": "/moolalahdigitalportfolio/static/js/main.df6a088a.chunk.js"
+  },
+  {
+    "revision": "fddf3d72a241309f3600",
+    "url": "/moolalahdigitalportfolio/static/js/runtime~main.e54d74f6.js"
+  },
+  {
+    "revision": "d37133dcff7cb41846ef",
+    "url": "/moolalahdigitalportfolio/static/css/2.b6ac7d14.chunk.css"
+  },
+  {
+    "revision": "d37133dcff7cb41846ef",
+    "url": "/moolalahdigitalportfolio/static/js/2.d551d02e.chunk.js"
+  },
+  {
+    "revision": "39b45f1d739d54d1747f",
+    "url": "/moolalahdigitalportfolio/static/js/3.bd123369.chunk.js"
+  },
+  {
+    "revision": "dce2e120d743bd765516b9e038d915d9",
+    "url": "/moolalahdigitalportfolio/static/media/alert.dce2e120.png"
+  },
+  {
+    "revision": "0fd70c878d9642c33263df5edeacc929",
+    "url": "/moolalahdigitalportfolio/static/media/progress1.0fd70c87.jpg"
+  },
+  {
+    "revision": "e6070e21e35851d10e8fbcf7643ec599",
+    "url": "/moolalahdigitalportfolio/static/media/progress2.e6070e21.png"
+  },
+  {
+    "revision": "39276e3b89f5257dd02f8627923865cb",
+    "url": "/moolalahdigitalportfolio/static/media/initialSketch.39276e3b.png"
+  },
+  {
+    "revision": "69097e75e97ee0018add1c4db110effc",
+    "url": "/moolalahdigitalportfolio/static/media/sketch.69097e75.png"
+  },
+  {
+    "revision": "bd1943211e24370ff015d6a6842999af",
+    "url": "/moolalahdigitalportfolio/static/media/final.bd194321.png"
+  },
+  {
+    "revision": "afa66fa4c2b1a78d0b84852ebc599a4a",
+    "url": "/moolalahdigitalportfolio/static/media/dashboard.afa66fa4.png"
+  },
+  {
+    "revision": "aa1df4a85d7bf344d730178f2061666f",
+    "url": "/moolalahdigitalportfolio/static/media/evaluation1.aa1df4a8.png"
+  },
+  {
+    "revision": "6cfb654ff964a545cb71d9e139fb20bb",
+    "url": "/moolalahdigitalportfolio/static/media/evaluation2.6cfb654f.png"
+  },
+  {
+    "revision": "8016004f2c6b8f6ec862150798868583",
+    "url": "/moolalahdigitalportfolio/index.html"
+  }
+];
